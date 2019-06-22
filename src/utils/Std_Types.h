ea023b10d7f4d8be5c3e3aed4e3c7ab0160163c7
Muhammad Elzeiny <Muhammad.Elzeiny@AVELABS.CAI>
2019-06-22 15:04:01 +0200

    add util folder

---
+++ src/utils/Std_Types.h
@@ -0,0 +1,36 @@
+/*
+ * Std_Types.h
+ *
+ *  Created on: Jun 22, 2019
+ *      Author: Muhammad.Elzeiny
+ */
+
+#ifndef UTILS_STD_TYPES_H_
+#define UTILS_STD_TYPES_H_
+
+typedef unsigned char uint8;
+typedef unsigned short int uint16;
+typedef unsigned long int uint32;
+
+typedef signed char sint8;
+typedef signed short int sint16;
+typedef signed long int sint32;
+
+typedef float float32;
+
+typedef enum
+{
+	STD_low,
+	STD_high
+}STD_levelType;
+
+typedef enum
+{
+	false,
+	true
+}boolean;
+
+
+
+
+#endif /* UTILS_STD_TYPES_H_ */
